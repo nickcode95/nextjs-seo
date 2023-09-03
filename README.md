60df62138d60f521cad8b5f3dcf20909736e4857
Nick Marie <nicholasm.marie@gmail.com>
2023-09-03 18:34:04 +1000

    add color and margin params to button component

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 This repo contains a next.js project for creating SEO ready websites built on React and leveraging Tailwind for CSS.
 
-## UI Components
+# UI Components
 
-# Button
-Contains an opinionated module for creating a button. Takes a parameter for button color and button border.
+## Button
+Contains an opinionated module for creating a button. Takes a parameter for button color, margin, witdth of border and border color.
--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -1,7 +1,7 @@
 export default function Button (props) {
     return (
-        <div>
-            <button className={`mx-auto ${props.color} text-sm ${props.border} py-1 px-2 rounded-full`}type="button">{props.text}</button>
-        </div>
+        <>
+            <button className={`mx-auto ${props.margin} ${props.color} text-sm border-4 ${props.borderColor} py-1 px-2 rounded-full`}type="button">{props.text}</button>
+        </>
     )
 }
